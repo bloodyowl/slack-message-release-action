7c37066b14f34af03e99f9bc89a652f8a9e8cda6
bloodyowl <bloodyowl@icloud.com>
2021-06-08 16:37:23 +0200

    WIP

---
+++ index.js
@@ -0,0 +1,29 @@
+let { IncomingWebhook } = require("@slack/webhook");
+
+let webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
+
+let repository = core.getInput("repository");
+let version = core.getInput("version");
+let changelog = core.getInput("changelog");
+
+function getChangelogForVersion(changelog, version) {
+  version = version.startsWith("v") ? version.slice(1) : version;
+  let startText = `## ${version}\n`;
+  let start = changelog.indexOf(startText);
+  if (start == -1) {
+    return "No changelog found";
+  }
+  let end = changelog.indexOf("\n##", start + startText.length);
+  return changelog.slice(start + startText.length, end - start).trim();
+}
+
+let formattedChangelog = getChangelogForVersion(changelog, version)
+  .split("\n")
+  .map((item) => `> ${item}`)
+  .join("\n");
+
+await webhook.send({
+  text: `${repository} ${version} has been [released](https://github.com/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}) ✨
+
+${formattedChangelog}`,
+});
