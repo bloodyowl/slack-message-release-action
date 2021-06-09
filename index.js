d135e929b9e8701e0bbdf6668ec96b2bd7210a65
bloodyowl <bloodyowl@icloud.com>
2021-06-09 18:37:31 +0200

    Node 12 only

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 let core = require("@actions/core");
 let slackMessageRelease = require("./slackMessageRelease.js");
 
-try {
-  await slackMessageRelease({
-    githubRepository: process.env.GITHUB_REPOSITORY,
-    version: core.getInput("version"),
-    changelog: core.getInput("changelog"),
-    githubRunId: process.env.GITHUB_RUN_ID,
-    slackWebhookUrl: core.getInput("slack_webhook_url"),
-  });
-  console.log("Message successfully sent 💬");
-} catch (err) {
-  console.error("The following error occured");
-  console.error(err);
-  process.exit(1);
-}
+slackMessageRelease({
+  githubRepository: process.env.GITHUB_REPOSITORY,
+  version: core.getInput("version"),
+  changelog: core.getInput("changelog"),
+  githubRunId: process.env.GITHUB_RUN_ID,
+  slackWebhookUrl: core.getInput("slack_webhook_url"),
+}).then(
+  () => {
+    console.log("Message successfully sent 💬");
+  },
+  (err) => {
+    console.error("The following error occured");
+    console.error(err);
+    process.exit(1);
+  }
+);
